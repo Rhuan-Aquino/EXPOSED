ebcf1f99b6fc038fc3f05619838af9d2d2a61c3b
Rhuan-Aquino <rhuandeaquino19@gmail.com>
2026-04-02 10:22:52 -0300

    fix: dynamic port for render deploy

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -21,9 +21,11 @@ app.get("/", (req, res) => {
 
 app.use("/auth", authRoutes); // 👈 ESSA LINHA RESOLVE TUDO
 
-app.listen(3001, () => {
-    console.log("🔥 SERVER SUBIU");
-    console.log("Server running on http://localhost:3001");
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log("🔥 SERVER SUBIU");
+  console.log(`Server running on port ${PORT}`);
 });
 
 
